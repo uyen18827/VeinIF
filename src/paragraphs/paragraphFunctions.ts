0c41e3766bbf7ebd053b1c798addb29d04c85827
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-01-01 21:45:30 +0700

    created showChoices() for better code reuse

--- src/paragraphs/paragraphFunctions.ts
+++ src/paragraphs/paragraphFunctions.ts
@@ -2,6 +2,31 @@ import { Paragraphs } from "../model/paragraph.js";
 import { player } from "../player/playerInfo.js";
 import { getParagraph } from "./allParagraphs.js";
 var currentParagraph: string | undefined;
+/**
+ * Show available choices in a paragraph
+ * @param choices should takes the value of allParagraphs[nextid].choices
+ * @param choiceContainer document.getElementById("choices");
+ */
+function showChoices(choices: any, choiceContainer: any){
+    if (choices) {
+        for (var i = 0; i < choices.length; i++) {
+            var currentChoice = choices[i];
+            var nextid: number = currentChoice.nextid;
+            let choice = `<a href="#" 
+            class="choices" id="n${nextid}" >
+            ${currentChoice.choiceCont} 
+            </a><br>`
+            choiceContainer.innerHTML += choice;
+        }
+        for (var i = 0; i < choices.length; i++) {
+            let currentChoice = choices[i];
+            let nextid: number = currentChoice.nextid;
+            let style = choices[i].style;
+            let choiceHTML = choiceContainer.querySelector("#n" + nextid);
+            choiceHTML.addEventListener('click', function () { updateParagraph(nextid, style) });
+        }
+    }
+}
 /**Get nextid, then show the paragraph with that id.
   * @param {number} nextid next paragraph's id.
   * @param {string} style optional. Update paragraph style. Leave blank for default: clear previous paragraph then show the next one.
@@ -18,27 +43,7 @@ export function updateParagraph(nextid: number, style?: string) {
             paragraphContainer.innerHTML = currentParagraph;
             choiceContainer.innerHTML = null;
             choices = allParagraphs[nextid].choices;
-
-            if (choices) {
-                for (var i = 0; i < choices.length; i++) {
-                    var currentChoice = choices[i];
-                    var nextid: number = currentChoice.nextid;
-                    let choice = `<a href="#" 
-                    class="choices" id="n${nextid}" >
-                    ${currentChoice.choiceCont} 
-                    </a><br>`
-                    choiceContainer.innerHTML += choice;
-                }
-                for (var i = 0; i < choices.length; i++) {
-                    let currentChoice = choices[i];
-                    console.log("currentChoice.nextid" + currentChoice.nextid)
-                    let nextid: number = currentChoice.nextid;
-                    let style = choices[i].style;
-                    console.log("nextid: " + nextid)
-                    let choiceHTML = choiceContainer.querySelector("#n" + nextid);
-                    choiceHTML.addEventListener('click', function () { updateParagraph(nextid, style) });
-                }
-            }
+            showChoices(choices, choiceContainer);
             break;
         default:
             paragraphContainer.innerHTML = null;
@@ -46,24 +51,7 @@ export function updateParagraph(nextid: number, style?: string) {
             currentParagraph = allParagraphs[nextid].content;
             paragraphContainer.innerHTML = currentParagraph;
             choices = allParagraphs[nextid].choices;
-            if (choices) {
-                for (var i = 0; i < choices.length; i++) {
-                    var currentChoice = choices[i];
-                    var nextid: number = currentChoice.nextid;
-                    let choice = `<a href="#" 
-                    class="choices" id="n${nextid}" >
-                    ${currentChoice.choiceCont} 
-                    </a><br>`
-                    choiceContainer.innerHTML += choice;
-                }
-                for (var i = 0; i < choices.length; i++) {
-                    let currentChoice = choices[i];
-                    let nextid: number = currentChoice.nextid;
-                    let choiceHTML = choiceContainer.querySelector("#n" + nextid);
-                    let style = choices[i].style;
-                    choiceHTML.addEventListener('click', function () { updateParagraph(nextid, style) });
-                }
-            }
+            showChoices(choices, choiceContainer);
             break;
     }
 }
