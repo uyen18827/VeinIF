25904cf23ae9a35557e0b81a7e48ddbdd42aa4e8
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-02-07 16:58:00 +0700

    added functions that handle stats on UI

--- dist/player/statInfos.js
+++ dist/player/statInfos.js
@@ -1,8 +1,8 @@
 import { Stat } from "../model/Stat.js";
-//Place the initial value of your player's Stat here.
+//Place the initial value of your player's Stat here, if you want to.
 const playerStat = [
-    { statName: "Intellect", value: 10 },
-    { statName: "Endurance", value: 4 }
+// { statName: "Intellect", value: 10 },
+// { statName: "Endurance", value: 4 }
 ];
 export function getStat() {
     return playerStat;
@@ -34,3 +34,38 @@ export function clearAllStat() {
 export function deleteStat(statName) {
     getStat().find(element => element.statName == statName);
 }
+//if stat already exist, add modify value, if not in playerStat, add to it.
+export function handleStats(stat) {
+    let found = getStat().find(element => element.statName = stat.statName);
+    if (found) {
+        modifyStatValue(found, stat.value);
+        updateStatHTML(found);
+        console.log(`Handled! modified`);
+    }
+    if (!found) {
+        addNewStat(stat.statName, stat.value);
+        appendStatHTML(stat);
+        console.log(`added new stat - Handled!`);
+    }
+}
+export function appendStatHTML(stat) {
+    let statContainer = document.querySelector(`.stat`);
+    statContainer.innerHTML += `<div id='stat-${stat.statName}'>${stat.statName}: ${stat.value}</div>`;
+}
+export function clearStatHTML() {
+    let statContainer = document.querySelector(`.stat`);
+    statContainer.textContent = ``;
+}
+/** Show all Stat on the UI.
+ * Use case: run when load game.
+ */
+export function showAllStatHTML(stat) {
+    stat.forEach(element => {
+        appendStatHTML(element);
+    });
+}
+export function updateStatHTML(stat) {
+    let statHTML = document.querySelector(`#stat-${stat.statName}`);
+    statHTML.innerHTML = `<div id='stat-${stat.statName}'>${stat.statName}: ${stat.value}</div>`;
+}
+//TODO: updateStatHTML: separate statName, stat value into two different HTML elements, preferably a table
--- src/player/statInfos.ts
+++ src/player/statInfos.ts
@@ -1,9 +1,9 @@
 import { Stat } from "../model/Stat.js";
 
-//Place the initial value of your player's Stat here.
+//Place the initial value of your player's Stat here, if you want to.
 const playerStat: Stat[] = [
-    { statName: "Intellect", value: 10 },
-    { statName: "Endurance", value: 4 }
+    // { statName: "Intellect", value: 10 },
+    // { statName: "Endurance", value: 4 }
 ]
 
 export function getStat() {
@@ -18,7 +18,6 @@ export function getStat() {
 export function addNewStat(statName: string, value: number) {
     let newStat = new Stat(statName, value);
     getStat().push(newStat);
-
 }
 
 /** Add or subtract point from a stat 
@@ -42,3 +41,43 @@ export function clearAllStat() {
 export function deleteStat(statName: Stat["statName"]) {
     getStat().find(element => element.statName == statName);
 }
+
+//if stat already exist, add modify value, if not in playerStat, add to it.
+export function handleStats(stat: Stat) {
+    let found = getStat().find(element => element.statName = stat.statName);
+    if (found) {
+        modifyStatValue(found, stat.value);
+        updateStatHTML(found);
+        console.log(`Handled! modified`)
+    }
+    if (!found) {
+        addNewStat(stat.statName, stat.value);
+        appendStatHTML(stat);
+        console.log(`added new stat - Handled!`)
+    }
+}
+
+export function appendStatHTML(stat: Stat) {
+    let statContainer = document.querySelector(`.stat`);
+    statContainer!.innerHTML += `<div id='stat-${stat.statName}'>${stat.statName}: ${stat.value}</div>`;
+}
+
+export function clearStatHTML() {
+    let statContainer = document.querySelector(`.stat`)!;
+    statContainer.textContent = ``;
+}
+
+/** Show all Stat on the UI.
+ * Use case: run when load game.
+ */
+export function showAllStatHTML(stat: Stat[]) {
+    stat.forEach(element => {
+        appendStatHTML(element);
+    });
+}
+
+export function updateStatHTML(stat: Stat) {
+    let statHTML = document.querySelector(`#stat-${stat.statName}`);
+    statHTML!.innerHTML = `<div id='stat-${stat.statName}'>${stat.statName}: ${stat.value}</div>`;
+}
+//TODO: updateStatHTML: separate statName, stat value into two different HTML elements, preferably a table
