f9bbf616ece5b938e9de41634943e0ab4cd9385a
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-02-07 18:57:46 +0700

    feat: added applyConsequence to handle consequence
    After a choice, player gain/loose item or stat point, or both.

---
+++ src/conditions/consequences.ts
@@ -0,0 +1,20 @@
+import { getItem } from "../inventory/inventory.js";
+import { Consequence } from "../model/paragraph.js";
+import { getCurrentParagraphID } from "../paragraphs/paragraphFunctions.js";
+import { handleStats } from "../player/statInfos.js";
+
+//Consequence: After a choice, player gain/loose item or stat point, or both.
+export function applyConsequence(consequence: Consequence) {
+    let item = consequence.item;
+    let stat = consequence.stat
+    if (item) {
+        item.forEach(item => {
+            getItem(item, getCurrentParagraphID());
+        });
+    }
+    if (stat) {
+        stat.forEach(stat => {
+            handleStats(stat);
+        });
+    }
+}
