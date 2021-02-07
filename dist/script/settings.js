d6728059687d5180dda0b8202ec5ca2331b348b0
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-02-07 17:27:05 +0700

    clear Stat from save and UI when restart game.

--- dist/script/settings.js
+++ dist/script/settings.js
@@ -1,13 +1,16 @@
 import { clearInventory, clearInventoryHTML } from "../inventory/inventory.js";
 import { updateParagraph } from "../paragraphs/paragraphFunctions.js";
 import { resetPlayer } from "../player/playerInfo.js";
+import { clearAllStat, clearStatHTML } from "../player/statInfos.js";
 import { autoSave } from "./saveScript.js";
 export function restartGame() {
     updateParagraph(0);
     clearInventory();
     resetPlayer();
-    autoSave();
     clearInventoryHTML();
+    clearAllStat();
+    clearStatHTML();
+    autoSave();
 }
 let gameInfo = {
     gameVersion: `0.0.1`,
@@ -16,5 +19,5 @@ let gameInfo = {
     description: ``,
     author: ``,
     engineVersion: `0.0.0.1`,
-    engine: `VellumEngine`
+    engine: ``
 };
--- src/script/settings.ts
+++ src/script/settings.ts
@@ -1,14 +1,17 @@
 import { clearInventory, clearInventoryHTML } from "../inventory/inventory.js";
 import { updateParagraph } from "../paragraphs/paragraphFunctions.js";
 import { resetPlayer } from "../player/playerInfo.js";
+import { clearAllStat, clearStatHTML } from "../player/statInfos.js";
 import { autoSave } from "./saveScript.js";
 
 export function restartGame() {
     updateParagraph(0);
     clearInventory();
     resetPlayer();
-    autoSave();
     clearInventoryHTML();
+    clearAllStat();
+    clearStatHTML();
+    autoSave();
 }
 
 export interface gameDetails{
@@ -28,5 +31,5 @@ let gameInfo: gameDetails = {
     description: ``,
     author: ``, //your name
     engineVersion: `0.0.0.1`,
-    engine: `VellumEngine`
+    engine: ``
 }
